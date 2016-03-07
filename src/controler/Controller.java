d35e1bd6acad8c7d13fb7bf042a0d4531ffaa40a
FabienneMarquis <fabienne.marquis@gmail.com>
2016-03-07 16:09:14 -0500

    blu

---
+++ src/controler/Controller.java
@@ -0,0 +1,22 @@
+package controler;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class Controller {
+
+    @FXML
+    private ListView<?> theList;
+
+    @FXML
+    private Button btnRoute;
+
+    @FXML
+    void getRouteOptimal(ActionEvent event) {
+
+    }
+
+}
+
